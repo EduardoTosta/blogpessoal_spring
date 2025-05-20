0b46ce05926a02d4e64ef93003754b8cf7357d5c
Eduardo Tosta <eduardo_tosta@outlook.com>
2025-05-20 16:25:18 -0300

    :sparkles: Implementação do relacionamento entre Tema e Postagem

--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,10 +1,17 @@
 package com.generation.blogpessoal.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -22,8 +29,12 @@ public class Tema {
 	@Column(length = 1000)
 	@NotBlank(message = "O atributo 'descrição' é obrigatório")
 	@Pattern(regexp = "^[^0-9].*", message = "A descrição não pode ser apenas numérico")
-	@Size(min = 10, max = 1000, message = "O atributo 'título' deve ter entre 5 e 100 caracteres")
+	@Size(min = 10, max = 1000, message = "O atributo 'descrição' deve ter entre 10 e 1000 caracteres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
 	
 
 	public Long getId() {
@@ -38,9 +49,18 @@ public class Tema {
 		return descricao;
 	}
 
-	public void getDescricao(String descricao) {
+	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	
 }
 
